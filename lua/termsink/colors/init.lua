af5ae80357ff732b69802e4a381aeea1759ed953
James Schurig <jnschurig@gmail.com>
2026-01-11 19:09:30 -0700

    fix

--- lua/termsink/colors/init.lua
+++ lua/termsink/colors/init.lua
@@ -1,5 +1,6 @@
 -- local high_visibility = require "termsink.util.config".settings.high_visibility
-local term_colors = require("termsink.colors.termcolor")
+-- local term_colors = require("termsink.colors.termcolor")
+local term_colors = require("termcolor")
 -- local closest_color_match = require("termsink.functions.functions.closest_color_match")
 -- local adjust_color_value = require("termsink.functions.functions.adjust_color_value")
 local functions = require("termsink.functions")
